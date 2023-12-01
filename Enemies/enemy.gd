ec2455adcc128f64c3eb5154450cf33415edd2cf
elbow667 <elbow667@gmail.com>
2023-12-01 08:30:56 -0500

    P9 Explosion Effect and Destory part 2

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var stats_component: = $StatsComponent as StatsComponent
 @onready var move_component: = $MoveComponent as MoveComponent
-@onready var visible_on_screen_notifier_2d: = $VisibleOnScreenNotifier2D
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 @onready var scale_component: = $ScaleComponent as ScaleComponent
 @onready var flash_component: = $FlashComponent as FlashComponent
 @onready var shake_component: = $ShakeComponent as ShakeComponent
