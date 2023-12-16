70cca9a212484daed5f550f03321b1910f847a4b
elbow667 <elbow667@gmail.com>
2023-12-16 12:54:53 -0500

    Part 13 Game Score and High Score

--- Menus/game_over.gd
+++ Menus/game_over.gd
@@ -1,6 +1,17 @@
 extends Control
 
+@export var game_stats: GameStats
 
+@onready var score_value: Label = %ScoreValue
+@onready var highscore_value: Label = %HighscoreValue
+
+func _ready() -> void:
+	if game_stats.score > game_stats.highscore:
+		game_stats.highscore = game_stats.score
+	score_value.text = str(game_stats.score)
+	highscore_value.text = str(game_stats.highscore)
+	
 func _process(delta) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
+		game_stats.score = 0
 		get_tree().change_scene_to_file("res://Menus/menu.tscn")
